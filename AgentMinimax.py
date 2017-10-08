3e68360f858725210d034c031c804083ae5930cc
zas97 <joanc1997@gmail.com>
2017-10-09 00:10:35 +0200

    first

---
+++ AgentMinimax.py
@@ -0,0 +1,44 @@
+from SimpleBoard import*
+import time
+import random
+class AgentMinimax:
+
+    def __init__(self,board,isWhite,profundity=6):
+        self.board=SimpleBoard(board)
+        self.white=isWhite
+        self.profundity=profundity
+        self.iteratorMoves=0
+        self.moves=[]
+
+    def negamax(self,profundity,white):
+        if profundity==0:
+            r=self.board.evalute()
+            if white:
+                return r
+            return -r
+        moves=self.board.allMoves(white)
+        bestMove=None
+        m=-40
+        for i in moves:
+            self.board.play(i)
+            aux=-self.negamax(profundity-1,not white)
+            if aux>m or (aux==m and bool(random.getrandbits(1))):
+                m=aux
+                bestMove=i
+            self.board.undo()
+        self.moves=bestMove
+        return m
+
+
+
+
+    def getMove(self):
+        self.iteratorMoves+=1
+        if self.iteratorMoves>=len(self.moves):
+            self.iteratorMoves=0
+            print(self.negamax(self.profundity,self.white))
+        time.sleep(0.5)
+        return self.moves[self.iteratorMoves]
+
+
+
